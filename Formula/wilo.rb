d60a01d70cf06f7a948b154af200a47fe380db62
Julien B <julien@julb.me>
2023-08-10 10:38:07 +0200

    fix: issue in formula

--- Formula/wilo.rb
+++ Formula/wilo.rb
@@ -1,11 +1,10 @@
 # Generated with JReleaser 1.3.1 at 2023-08-10T08:28:58.176445902Z
 class Wilo < Formula
-  desc "This project provides a logging utility for Bash scripts."
+  desc "This project provides a logging utility for Bash scripts"
   homepage "https://julbme.github.io/wilo/"
   url "https://github.com/julbme/wilo/releases/download/v1.0.2/wilo-1.0.2.zip"
-  version "1.0.2"
   sha256 "e4c998f208b441376ba0aed44b3ac012e4df4a3b49c02d66ab91faef62d2c2a7"
-  license "MIT License"
+  license "MIT"
 
   depends_on "jq"
 
@@ -16,6 +15,6 @@ class Wilo < Formula
 
   test do
     output = shell_output("#{bin}/wilo --version")
-    assert_match /^wilo 1.0.2 /, output
+    assert_match "/^wilo 1.0.2 /", output
   end
 end
