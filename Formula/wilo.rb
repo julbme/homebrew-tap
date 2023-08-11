0ee4009554fc06bba208503b832cdbfe20564a6b
julb <julien@julb.me>
2023-08-11 10:02:58 +0000

    wilo v1.0.3

--- Formula/wilo.rb
+++ Formula/wilo.rb
@@ -1,9 +1,9 @@
-# Generated with JReleaser 1.3.1 at 2023-08-10T08:28:58.176445902Z
+# Generated with JReleaser 1.3.1 at 2023-08-11T10:02:42.604640131Z
 class Wilo < Formula
   desc "This project provides a logging utility for Bash scripts"
   homepage "https://julbme.github.io/wilo/"
-  url "https://github.com/julbme/wilo/releases/download/v1.0.2/wilo-1.0.2.zip"
-  sha256 "e4c998f208b441376ba0aed44b3ac012e4df4a3b49c02d66ab91faef62d2c2a7"
+  url "https://github.com/julbme/wilo/releases/download/v1.0.3/wilo-1.0.3.zip"
+  sha256 "a2774b0bc7b4d3fd3013719d16529abff34dd7c389fd85a8f1ac1f46e9ea374e"
   license "MIT"
 
   depends_on "jq"
@@ -15,6 +15,6 @@ class Wilo < Formula
 
   test do
     output = shell_output("#{bin}/wilo --version")
-    assert_match "/^wilo 1.0.2 /", output
+    assert_match "/^wilo 1.0.3 /", output
   end
 end
